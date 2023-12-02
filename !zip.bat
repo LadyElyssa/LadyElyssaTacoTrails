e4418a97c0f1a7cbacb38371136764ab201107f7
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2023-12-02 15:06:00 +0000

    Quick Zip for GW2TacO Trails
    * Now checks for existing Zip File and removes in case interrupted before completion.

--- !zip.bat
+++ !zip.bat
@@ -1,3 +1,4 @@
+@IF EXIST "LadyElyssa.zip" del \Q "LadyElyssa.zip"
 @IF EXIST "LadyElyssa.taco" del \Q "LadyElyssa.taco"
 @powershell Compress-Archive "'Data\'","*.xml" LadyElyssa.zip -Force
 @ren LadyElyssa.zip LadyElyssa.taco
