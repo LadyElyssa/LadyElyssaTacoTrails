51b3bbacc28508e6a3e715b68aabc6cacd11cf78
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2024-01-24 11:30:38 +0000

    Quick Zip for GW2TacO Trails
    * Now removes GW2TacO.log file.

--- !zip.bat
+++ !zip.bat
@@ -1,3 +1,4 @@
+@IF EXIST "E:\Games\GW2TacO\GW2TacO.log" del \Q "E:\Games\GW2TacO\GW2TacO.log"
 @IF EXIST "LadyElyssa.zip" del \Q "LadyElyssa.zip"
 @IF EXIST "LadyElyssa.taco" del \Q "LadyElyssa.taco"
 @powershell Compress-Archive "'Data\'","*.xml" LadyElyssa.zip -Force
