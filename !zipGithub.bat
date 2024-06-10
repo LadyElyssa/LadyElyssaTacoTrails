461bcad98f00cd3e580ace150ddcc0b4e697e85f
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2024-06-10 12:16:50 +0100

    Delete !zipGithub.bat

--- !zipGithub.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-XCOPY /S/E/Y/Q *.bat "E:\Github\LadyElyssaTacoTrails"
-XCOPY /S/E/Y/Q *.md "E:\Github\LadyElyssaTacoTrails"
-XCOPY /S/E/Y/Q *.xml "E:\Github\LadyElyssaTacoTrails"
-XCOPY /T/S/E/Y/Q Data\ "E:\Github\LadyElyssaTacoTrails\Data\"
-
-:Done
