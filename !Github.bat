19565391f7115a457bcc6a032eded5bc799d1f84
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2024-06-24 15:43:47 +0100

    Updates Working Files to Github Desktop

--- !Github.bat
+++ !Github.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
+XCOPY /E/Y/I/Q .\Data\*.* "E:\Github\LadyElyssaTacoTrails\Data"
 XCOPY /S/E/Y/Q *.bat "E:\Github\LadyElyssaTacoTrails"
 XCOPY /S/E/Y/Q *.md "E:\Github\LadyElyssaTacoTrails"
 XCOPY /S/E/Y/Q *.xml "E:\Github\LadyElyssaTacoTrails"
-XCOPY /T/S/E/Y/Q Data\ "E:\Github\LadyElyssaTacoTrails\Data\"
 
 :Done
