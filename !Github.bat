5fb6f1741b8ecdf93cfc75181a47121fbdaa9c52
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2025-03-16 15:57:14 +0000

    Github.bat

--- !Github.bat
+++ !Github.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
+@IF EXIST "E:\Games\GW2TacO\GW2TacO.log" (
+  del /Q "E:\Games\GW2TacO\GW2TacO.log"
+)
 XCOPY /E/Y/I/Q .\Data\*.* "E:\Github\LadyElyssaTacoTrails\Data"
 XCOPY /S/E/Y/Q *.bat "E:\Github\LadyElyssaTacoTrails"
 XCOPY /S/E/Y/Q *.md "E:\Github\LadyElyssaTacoTrails"
