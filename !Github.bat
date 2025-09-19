4c5fd7dd5c00d6fb42da0166462799e6d099c151
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2025-09-19 19:23:48 +0100

    Github.bat

--- !Github.bat
+++ !Github.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
-@IF EXIST "%temp%\gw2cache*." FOR /d %%G in ("%temp%\GW2Cache*") DO RMDIR /s /q "%%G"
 @IF EXIST "E:\Games\GW2TacO\GW2TacO.log" (
   del /Q "E:\Games\GW2TacO\GW2TacO.log"
 )
+@IF EXIST "E:\Documents\Guild Wars 2\addons\blishhud\logs" del /Q "E:\Documents\Guild Wars 2\addons\blishhud\logs\*.log"
+@IF EXIST "%temp%\gw2cache*." FOR /d %%G in ("%temp%\GW2Cache*") DO RMDIR /s /q "%%G"
 XCOPY /E/Y/I/Q .\Data\*.* "E:\Github\LadyElyssaTacoTrails\Data"
 XCOPY /S/E/Y/Q *.bat "E:\Github\LadyElyssaTacoTrails"
 XCOPY /S/E/Y/Q *.md "E:\Github\LadyElyssaTacoTrails"
