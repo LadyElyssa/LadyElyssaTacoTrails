93fd50cebc526e0d41df2537af81e81d2bc4fa0d
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2025-11-17 15:34:58 +0000

    XML Check in Python

---
+++ xmlchk.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Usage: python xmlchk.py
+"""
+
+import pathlib
+import xml.etree.ElementTree as ET
+
+def check_file(path: pathlib.Path):
+    try:
+        ET.parse(path)
+    except Exception as e:
+        print(f"{path.name}: FAILED")
+        print(f"{type(e).__module__}.{type(e).__name__}: {e}\n")
+    """
+    else:
+        print(f"{path.name}: OK")
+    """
+
+def main():
+    p = pathlib.Path('.')
+    xml_files = sorted(p.glob('*.xml'))
+    for f in xml_files:
+        check_file(f)
+
+if __name__ == "__main__":
+    main()
